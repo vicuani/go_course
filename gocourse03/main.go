33bcebe399ac1edad8a3d69fb864ee9b68aa22b6
Viktor Anisimov <vicuani@gmail.com>
2024-08-25 21:55:44 +0200

    Next stage

--- gocourse03/main.go
+++ gocourse03/main.go
@@ -42,32 +42,16 @@ type (
 const labyrinthSize = 10
 const rodentMaxCount = 20
 
-// rodentType
-type rodentType int
-
-const (
-	Beaver rodentType = iota
-	Capybara
-	Mouse
-	Rat
-	Squirrel
-	RodentTypesCount // virtual element used to determine 'enum' size
-)
+type rodentType string
 
 func (r rodentType) String() string {
-	switch r {
-	case Beaver:
-		return "Beaver"
-	case Capybara:
-		return "Capybara"
-	case Mouse:
-		return "Mouse"
-	case Rat:
-		return "Rat"
-	case Squirrel:
-		return "Squirrel"
-	}
-	return "UNKNOWN"
+	return string(r)
+}
+
+var rodentTypes = [5]rodentType{"Beaver", "Capybara", "Mouse", "Rat", "Squirrel"}
+
+func generateRandomRodentType() rodentType {
+	return rodentTypes[rand.IntN(len(rodentTypes))]
 }
 
 type coordinates struct {
@@ -75,25 +59,71 @@ type coordinates struct {
 	y int
 }
 
-type rodent struct {
-	id          int
-	tp          rodentType
-	startCoords coordinates
-	endCoords   coordinates
-	//	TODO add movements
+func generateRandomCoordinates() coordinates {
+	return coordinates{
+		x: rand.IntN(labyrinthSize),
+		y: rand.IntN(labyrinthSize),
+	}
 }
 
-func newRodent(id int, tp rodentType, coords coordinates) *rodent {
+type sector struct {
+	leftTop     coordinates
+	rightBottom coordinates
+	rodents     []*rodent
+}
+
+func newSector(lt coordinates, rb coordinates) *sector {
+	return &sector{
+		leftTop:     lt,
+		rightBottom: rb,
+		rodents:     []*rodent{},
+	}
+}
+
+func (s *sector) areCoordinatedInSector(c coordinates) bool {
+	return c.x >= s.leftTop.x && c.x <= s.rightBottom.x && c.y >= s.leftTop.y && c.y <= s.rightBottom.y
+}
+
+func (s *sector) addRodent(r *rodent) {
+	s.rodents = append(s.rodents, r)
+}
+
+func (s *sector) removeRodent(r *rodent) {
+	for i, rod := range s.rodents {
+		if rod.id == r.id {
+			s.rodents = append(s.rodents[:i], s.rodents[i+1:]...)
+			return
+		}
+	}
+}
+
+func findSector(sectors []*sector, c coordinates) *sector {
+	for _, s := range sectors {
+		if s.areCoordinatedInSector(c) {
+			return s
+		}
+	}
+
+	fmt.Println("Impossible situation, sector must be found")
+	return nil
+}
+
+type rodent struct {
+	id        int
+	tp        rodentType
+	movements []coordinates
+}
+
+func newRodent(id int) *rodent {
 	return &rodent{
-		id:          id,
-		tp:          tp,
-		startCoords: coords,
-		endCoords:   coords,
+		id:        id,
+		tp:        generateRandomRodentType(),
+		movements: []coordinates{generateRandomCoordinates()},
 	}
 }
 
 func (r *rodent) String() string {
-	return fmt.Sprintf("id = %v, type = %v, start coords = %v, end coords = %v", r.id, r.tp.String(), r.startCoords, r.endCoords)
+	return fmt.Sprintf("id = %v, type = %v, movements = %v", r.id, r.tp, r.movements)
 }
 
 var allMoves = []coordinates{
@@ -123,22 +153,33 @@ func makeMove(coords coordinates) coordinates {
 		choice := rand.IntN(len(validMoves))
 		move := validMoves[choice]
 		return coordinates{coords.x + move.x, coords.y + move.y}
-	} else {
-		fmt.Println("Impossible situation – no valid moves")
-		return coordinates{}
 	}
+
+	fmt.Println("Impossible situation – no valid moves")
+	return coordinates{}
 }
 
 func main() {
+	lCenter := labyrinthSize / 2
+	//	meaning that we have 4 same-size sectors
+	//	(0, 0) -> (4, 4), (5, 0) -> (9, 4)
+	//	(5, 0) -> (5, 4), (5, 5) -> (9, 9)
+	sectors := []*sector{
+		newSector(coordinates{x: 0, y: 0}, coordinates{x: lCenter - 1, y: lCenter - 1}),
+		newSector(coordinates{x: lCenter, y: 0}, coordinates{x: labyrinthSize - 1, y: lCenter - 1}),
+		newSector(coordinates{x: 0, y: lCenter - 1}, coordinates{x: lCenter - 1, y: labyrinthSize - 1}),
+		newSector(coordinates{x: lCenter, y: lCenter - 1}, coordinates{x: labyrinthSize - 1, y: labyrinthSize - 1}),
+	}
+
 	movesCount := 24 * 60
-	//	NOTE at least 1 rodent must be created
+	// at least 1 rodent must be created
 	rodentCount := rand.IntN(rodentMaxCount-1) + 1
 	fmt.Printf("Create %v rodents\n", rodentCount)
 	rodents := make([]*rodent, 0, rodentCount)
 
-	//	NOTE key – time, value – slice of rodents that were on sensor at this moment
+	// key – time, value – slice of rodents that were on sensor at this moment
 	// actually it can be
-	// [time: 3, id: 2], [time: 7, id: 17, 19], [time: 35, id: 2, 3, 5]
+	// [time: 3, id: 2], [time: 7, id: 17, 19], [time: 21, id: 2, 3, 5]
 	sensorHistory := make(map[int][]*rodent)
 
 	sensorCoords := coordinates{
@@ -147,34 +188,58 @@ func main() {
 	}
 
 	for i := 0; i < rodentCount; i++ {
-		tp := rodentType(rand.IntN(int(RodentTypesCount)))
-		coords := coordinates{
-			x: rand.IntN(labyrinthSize),
-			y: rand.IntN(labyrinthSize),
-		}
-
-		r := newRodent(i+1, tp, coords)
+		r := newRodent(i + 1)
 		rodents = append(rodents, r)
+		sector := findSector(sectors, r.movements[0])
+		if sector != nil {
+			sector.addRodent(r)
+		}
 	}
 
 	for i := 0; i < movesCount; i++ {
 		for _, rodent := range rodents {
-			newCoords := makeMove(rodent.endCoords)
+			lastCoords := rodent.movements[len(rodent.movements)-1]
+			newCoords := makeMove(lastCoords)
 			if newCoords == sensorCoords {
 				sensorHistory[i] = append(sensorHistory[i], rodent)
 			}
-			rodent.endCoords = newCoords
+			rodent.movements = append(rodent.movements, newCoords)
+
+			currentSector := findSector(sectors, lastCoords)
+			newSector := findSector(sectors, newCoords)
+
+			if currentSector != newSector {
+				if currentSector != nil {
+					currentSector.removeRodent(rodent)
+				}
+				if newSector != nil {
+					newSector.addRodent(rodent)
+				}
+			}
 		}
 	}
 
+	fmt.Println("\nMain sensor history:")
 	for key, value := range sensorHistory {
 		var rodStr string
-		for j, rod := range value {
-			rodStr += fmt.Sprintf("%v : %v; ", j, rod.String())
+		for _, rod := range value {
+			rodStr += fmt.Sprintf("%v, ", rod.id)
 		}
 		tm := time.Duration(key) * time.Minute
-		fmt.Printf("time = %s,\t\tvalue = %v\n", tm, value)
+		fmt.Printf("time = %-10s\trodent ids = %v\n", tm, rodStr)
 	}
 
-	//	TODO print all rodent + it's start position
+	fmt.Println("\nInformation about all sectors:")
+	for i, sector := range sectors {
+		fmt.Printf("Sector %d (%v - %v) contains rodents: ", i+1, sector.leftTop, sector.rightBottom)
+		for _, r := range sector.rodents {
+			fmt.Printf("%d ", r.id)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("\nAll rodents and their positions:")
+	for _, rodent := range rodents {
+		fmt.Printf("Rodend #%-2v, type: %-10v: start position: %v, end position: %v\n", rodent.id, rodent.tp, rodent.movements[0], rodent.movements[len(rodent.movements) - 1])
+	}
 }
