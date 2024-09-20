c55529acb9fa1868cc240e82ed4d3dde55ec4c0a
Viktor Anisimov <vicuani@gmail.com>
2024-09-20 21:16:23 +0200

    Final review fix

--- gocourse11/server/server.go
+++ gocourse11/server/server.go
@@ -6,21 +6,6 @@ import (
 	"github.com/vicuani/go_course/gocourse11/filter"
 )
 
-type Aquarium interface {
-	Size() int
-	Animal() string
-	SaltLevel() float64
-	Contaminants() float64
-	FilterSpeed() float64
-	Cleaners() float64
-
-	IncreaseFiltration(coef float64)
-	DecreaseFiltration(coef float64)
-	AddSalt(coef float64)
-	AddCleaners(coef float64)
-	CalculatePollution() float64
-}
-
 type Server struct {
 	filters   []filter.Filter
 }
