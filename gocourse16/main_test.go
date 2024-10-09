1394479fada0ede6408df3a172eb502406203aa4
Viktor Anisimov <vicuani@gmail.com>
2024-10-09 21:04:20 +0200

    Final review fix

--- gocourse16/main_test.go
+++ gocourse16/main_test.go
@@ -7,14 +7,12 @@ import (
 )
 
 func TestProcessContacts(t *testing.T) {
-	input :=
-`John Doe - +380 (99) 123-45-67
+	input := `John Doe - +380 (99) 123-45-67
 Jane Smith - (067) 987 65 43
 Joe Biden - +380///44/1///11/335/1
 `
 
-	expectedOutput :=
-`Name: John Doe, Phone: (099) 12-34-567
+	expectedOutput := `Name: John Doe, Phone: (099) 12-34-567
 Name: Jane Smith, Phone: (067) 98-76-543
 Name: Joe Biden, Phone: (044) 11-13-351
 `
@@ -29,5 +27,27 @@ Name: Joe Biden, Phone: (044) 11-13-351
 
 	if writer.String() != expectedOutput {
 		t.Errorf("unexpected output:\n got: %v\n want: %v", writer.String(), expectedOutput)
+	}
+}
+
+func TestProcessContacts_InvalidPhoneFormat(t *testing.T) {
+	input := `John Doe - 12345`
+	reader := strings.NewReader(input)
+	var writer bytes.Buffer
+
+	err := processContacts(reader, &writer)
+	if err == nil {
+		t.Fatal("expected error for invalid phone format, but got none")
+	}
+}
+
+func TestProcessContacts_InvalidNameFormat(t *testing.T) {
+	input := `Xi0442223334`
+	reader := strings.NewReader(input)
+	var writer bytes.Buffer
+
+	err := processContacts(reader, &writer)
+	if err == nil {
+		t.Fatal("expected error for invalid name format, but got none")
 	}
 }
